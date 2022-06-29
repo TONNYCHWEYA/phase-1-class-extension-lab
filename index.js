978ec236131eb45551d4635ce5ae3ff91f594417
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-29 15:16:54 +0300

    commit tree 06e66e88df93074ca5132272420616d2e539a0a1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Polygon{
+  constructor(array){
+    this.array = array
+  }
+  get countSides(){
+    return this.array.length
+  }
+  get perimeter(){
+    return this.array.reduce((total, item) => total + item)
+  }
+}
+class Triangle extends Polygon{
+  get isValid(){
+    const sum = this.array[0] + this.array[1]
+    return (sum > this.array[2])
+  }
+}
